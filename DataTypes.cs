3868fa960efec04cfcda3ff634af3ae027a143a9
SwathikaBollam <swathikareddy999@gmail.com>
2024-03-26 10:42:59 +0530

    initial commit

---
+++ DataTypes.cs
@@ -0,0 +1,52 @@
+using System;
+namespace consoleApp1{
+class DataTypes
+{
+static void Main(string[] args){
+char a='G';
+int i=50;
+byte b=255;
+sbyte b1=-122;
+short s=455;
+ushort s1=64878;
+uint i1=6788;
+long l=-789900000;
+ulong  l1=8999000000;
+float f=3.756777f;
+double d=8.35677;
+decimal dec=677.5m;
+Console.WriteLine(a);
+Console.WriteLine(i);
+
+Console.WriteLine(b);
+
+Console.WriteLine(b1);
+
+Console.WriteLine(s);
+
+Console.WriteLine(s1);
+
+Console.WriteLine(i1);
+
+Console.WriteLine(i);
+
+Console.WriteLine(d);
+Console.WriteLine(dec);
+
+Console.WriteLine(l);
+
+Console.WriteLine(l1);
+
+Console.WriteLine(f);
+int n=10;
+int* p=&n;
+Console.WriteLine("value:{0}",n);
+Console.WriteLine("Address:{0}",(int)p);
+}
+}
+}
+
+
+
+
+
